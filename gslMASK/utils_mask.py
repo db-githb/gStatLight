c10a4adf0df3fb5d2b526e99dff764ef5f428d8d
db-githb <snotboogieisdead@gmail.com>
2025-10-03 22:57:28 -0400

    removed debug statement

--- gslMASK/utils_mask.py
+++ gslMASK/utils_mask.py
@@ -16,7 +16,6 @@ def setup_mask(data_dir):
   processor = AutoProcessor.from_pretrained(model_id)
   dino = AutoModelForZeroShotObjectDetection.from_pretrained(model_id).to("cuda").eval()
   root = data_dir.parent.parent / "models"
-  print(root)
   root.mkdir(parents=True, exist_ok=True)
   sam2_checkpoint = root / "sam2.1_hiera_large.pt"
   model_cfg = "configs/sam2.1/sam2.1_hiera_l.yaml"
